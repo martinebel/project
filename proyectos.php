4b9c379af358f61f030a3110c3aa8290d5f4fde0
martinebel <36831742+martinebel@users.noreply.github.com>
2019-10-17 14:08:30 -0300

    project detail

--- proyectos.php
+++ proyectos.php
@@ -17,8 +17,8 @@ include 'header.php';
       				<div class="card-header">
       					<h4 class="card-title">Todos los Proyectos</h4>
       					<div class="heading-elements mt-0">
-      						<button class="btn btn-primary btn-sm "><i class="d-md-none d-block ft-plus white"></i>
-      							<span class="d-md-block d-none">Crear Proyecto</span></button>
+      						<a href="createProject.php" class="btn btn-primary btn-sm "><i class="d-md-none d-block ft-plus white"></i>
+      							<span class="d-md-block d-none">Crear Proyecto</span></a>
       					</div>
       				</div>
       			</div>
@@ -34,6 +34,27 @@ include 'header.php';
                       </tr>
                     </thead>
                     <tbody id="listaproyectos"></tbody>
+                    <?php
+                    $stmt = $dbh->prepare("select * from projects");
+                    $stmt->execute();
+                    $result = $stmt->fetchAll();
+                    foreach($result as $row){
+                      echo '<tr>
+                      <td>'.$row["project_id"].'</td>
+                      <td><a href="viewProject.php?id='.$row["project_id"].'">'.$row["name"].'</a></td>
+                      <td>'.$row["date_creation"].'</td>';
+                      $status='';
+                      switch($row["status"])
+                      {
+                        case 'OPEN': $status='<span class="badge badge-success">Abierto</span>'; break;
+                        case 'CLOSED': $status='<span class="badge badge-warning">Cerrado</span>'; break;
+                        case 'HOLD': $status='<span class="badge badge-danger">En Espera</span>'; break;
+                        case 'PROGRESS': $status='<span class="badge badge-secondary">En Progreso</span>'; break;
+                      }
+                      echo '<td>'.$status.'</td></tr>';
+                    }
+
+                     ?>
                 </table>
       				</div>
       			</div>
@@ -55,4 +76,3 @@ include 'footer.php';
 <script src="app-assets/vendors/js/tables/dataTables.bootstrap4.min.js"></script>
 <script src="app-assets/vendors/js/tables/dataTables.responsive.min.js"></script>
 <script src="app-assets/vendors/js/tables/dataTables.rowReorder.min.js"></script>
-<script src="app-assets/actions/js/projects.js"></script>
